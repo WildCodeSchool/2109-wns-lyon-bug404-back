247e1675934ab4573386bd1d481076b15a1660aa
amira ZOUAK <zouak.amira@gmail.com>
2021-11-23 15:59:45 +0100

    database connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,12 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const config =  
-{
-  'server' : process.env.SERV,
-  'host' : process.env.DB_HOST,
-  'db' : process.env.DB,
-  'db_uname' : process.env.DB_USER,
-  'db_password' : process.env.DB_PASS,
-  'port' : process.env.DB_PASS,
-}
+export const config = {
+	server: process.env.SERVER,
+	host: process.env.DB_HOST,
+	db: process.env.DB,
+	db_uname: process.env.DB_USER,
+	db_password: process.env.DB_PASSWORD,
+	port: process.env.PORT,
+};
--- src/server.ts
+++ src/server.ts
@@ -7,23 +7,24 @@ import { config } from "./config/config";
 import { TaskResolver } from "./resolvers/TaskResolver";
 
 export async function bootstrap() {
-  await createConnection({
-    type: config.server as "mysql",
-    url: `${config.server}://${config.db_uname}:${config.db_password}@${config.host}/${config.db}`,
-    entities: [path.resolve(__dirname,'./models/*.{ts,js}')],
-    synchronize: true,
-  });
+	console.log(config);
+	await createConnection({
+		type: config.server as "mariadb",
+		url: `${config.server}://${config.db_uname}:${config.db_password}@${config.host}/${config.db}`,
+		entities: [path.resolve(__dirname, "./models/*.{ts,js}")],
+		synchronize: true,
+	});
 
-  const schema = await buildSchema({
-    resolvers: [TaskResolver],
-  });
+	const schema = await buildSchema({
+		resolvers: [TaskResolver],
+	});
 
-  const server = new ApolloServer({
-    schema,
-  });
+	const server = new ApolloServer({
+		schema,
+	});
 
-  const { url } = await server.listen(config.port);
-  console.log(`Server is running, GraphQL Playground available at ${url}`);
+	const { url } = await server.listen(config.port);
+	console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
 
 bootstrap();
